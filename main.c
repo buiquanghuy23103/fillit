d6f8e574ff72fea18b419579e227514aca4a8b3d
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-26 23:09:09 +0200

    fix norm

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 22:26:25 by hbui              #+#    #+#             */
-/*   Updated: 2021/12/26 21:17:43 by hbui             ###   ########.fr       */
+/*   Updated: 2021/12/26 23:08:53 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-void	ft_print_error()
+void	ft_print_error(void)
 {
 	ft_putstr("error\n");
 }
@@ -20,6 +20,7 @@ void	ft_print_error()
 void	ft_solve(int fd)
 {
 	char	*line;
+
 	get_next_line(fd, &line);
 }
 
