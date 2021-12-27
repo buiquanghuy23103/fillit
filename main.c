f6601372b6033b92837de751a21361e20fd59d78
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-27 08:55:07 +0200

    print error when file is empty

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 22:26:25 by hbui              #+#    #+#             */
-/*   Updated: 2021/12/27 08:24:42 by hbui             ###   ########.fr       */
+/*   Updated: 2021/12/27 08:51:00 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,27 @@ void	ft_handle_error(void)
 void	ft_solve(int fd)
 {
 	char	*line;
-	int		ret;
+	int		a;
+	int		height;
 
-	while (ft_set(&ret, get_next_line(fd, &line)))
+	height = 0;
+	while (ft_set(&a, get_next_line(fd, &line)))
 	{
-		if (ret == -1)
+		if (a == -1)
+			ft_handle_error();
+		height++;
+		a = 0;
+		while (line[a])
+		{
+			if (a > 4)
+				ft_handle_error();
+			a++;
+		}
+		if (a < 4)
 			ft_handle_error();
 	}
+	if (height == 0)
+		ft_handle_error();
 }
 
 int	main(int argc, char **argv)
