1067acaaf8c20904ea2b13c2183373bf215dce86
jpikkuma <jpikkuma@student.hive.fi>
2022-01-04 23:22:36 +0200

    fixed a mistake in validator

--- preparation.c
+++ preparation.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   preparation.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
+/*   By: jpikkuma <jpikkuma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 15:42:27 by hbui              #+#    #+#             */
-/*   Updated: 2022/01/04 15:54:45 by hbui             ###   ########.fr       */
+/*   Updated: 2022/01/04 23:20:57 by jpikkuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,7 +80,7 @@ static void	ft_set_storage(t_tetr *storage, int *tmp)
 	{
 		if (tmp[i])
 		{
-			value |= 1 << i;
+			value |= 1 << (15 - i);
 			storage->tmino[storage->tcount][i / 4] |= 1 << (3 - (i % 4));
 		}
 		++i;
