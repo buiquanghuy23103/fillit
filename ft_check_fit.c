3ccb402f7eb3fe322aa9844d6cfa08c1037b7b93
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-01-07 17:29:31 +0200

    fix ft_check_fit

--- ft_check_fit.c
+++ ft_check_fit.c
@@ -6,7 +6,7 @@
 /*   By: jpikkuma <jpikkuma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 00:28:10 by jpikkuma          #+#    #+#             */
-/*   Updated: 2022/01/07 10:54:27 by hbui             ###   ########.fr       */
+/*   Updated: 2022/01/07 17:29:03 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,19 @@
 int	ft_check_fit(int *dst, int *src)
 {
 	int	i;
+	int	srow;
 	int	square_size;
 	int	height;
 
-	i = src[SROW];
+	i = 0;
+	srow = src[SROW];
 	square_size = src[SIZE];
 	height = src[HEIGHT];
 	if (height > square_size)
 		return (0);
-	while (i < square_size && src[i] != 0)
+	while (i < height)
 	{
-		if (src[i] & dst[i])
+		if (src[i] & dst[srow + i])
 			return (0);
 		++i;
 	}
