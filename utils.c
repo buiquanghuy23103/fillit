79eeaee05a5aaeb0fc212920467082fbbf6e88ba
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-01-08 13:59:23 +0200

    fix ft_place_piece

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: jpikkuma <jpikkuma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 15:46:57 by hbui              #+#    #+#             */
-/*   Updated: 2022/01/08 02:31:52 by jpikkuma         ###   ########.fr       */
+/*   Updated: 2022/01/08 13:58:27 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,6 +71,13 @@ void	ft_remove_piece(int *dst, int *src)
 
 void	ft_place_piece(int *dst, int *src)
 {
+	int	col;
+
+	col = src[SCOL];
 	while (*src)
-		*dst++ |= *src++;
+	{
+		*dst |= (*src >> col);
+		src++;
+		dst++;
+	}
 }
